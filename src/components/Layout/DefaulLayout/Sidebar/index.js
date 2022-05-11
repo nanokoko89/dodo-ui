bd7088fcb8db936f318b73bac2fb60e6997008f7
nanokok89 <nguyentuan1989xk@gmail.com>
2022-05-11 20:31:28 +0700

    commit on 'Dung khung layout mac dinh

--- src/components/Layout/DefaulLayout/Sidebar/index.js
+++ src/components/Layout/DefaulLayout/Sidebar/index.js
@@ -1,7 +1,8 @@
-import React from "react";
-
+import styles from "./Sidebar.module.scss";
+import classNames from "classnames/bind";
+const cx = classNames.bind(styles);
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return <aside className={cx("wrapper")}>Sidebar</aside>;
 }
 
 export default Sidebar;
--- src/components/Layout/DefaulLayout/index.js
+++ src/components/Layout/DefaulLayout/index.js
@@ -1,13 +1,15 @@
+import classNames from "classnames/bind";
 import Header from "~/components/Layout/components/Header";
+import styles from "./DefaulLayout.module.scss";
 import Sidebar from "./Sidebar";
-
+const cx = classNames.bind(styles);
 export default function DefaulLayout({ children }) {
   return (
-    <div>
+    <div className={cx("wrapper")}>
       <Header />
-      <div className="container">
+      <div className={cx("container")}>
         <Sidebar />
-        <div className="content">{children}</div>
+        <div className={cx("content")}>{children}</div>
       </div>
     </div>
   );
