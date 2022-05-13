07a7e45f3abaac496e9b420f5fe7c6e27e35b27e
nanokok89 <nguyentuan1989xk@gmail.com>
2022-05-13 09:42:16 +0700

    commit delete logo wrapper

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -13,9 +13,7 @@ function Header() {
   return (
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
-        <div className={cx("logo")}>
-          <img src={images.logo} alt="logo" />
-        </div>
+        <img src={images.logo} alt="logo" />
         <div className={cx("search")}>
           <input placeholder="Search accounts and videos" spellCheck={false} />
           <button className={cx("clear-btn")}>
